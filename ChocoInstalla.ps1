54b98275124333ecf53dc46d4199d556d6a9f76d
Federico Rota <federico.rota01@gmail.com>
2012-11-28 15:05:12 +0100

    First commit

---
+++ ChocoInstalla.ps1
@@ -0,0 +1,25 @@
+# In PowerShell settare
+# Set-ExecutionPolicy Unrestricted
+
+# Chocolatey
+iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
+
+cinst KickAssVim
+cinst GoogleChrome
+cinst skype
+cinst git.install
+cinst notepadplusplus.install
+cinst ferventcoder.chocolatey.utilities
+cinst keepassx
+cinst dropbox
+cinst windirstat
+cinst winrar
+cinst Firefox
+cinst toggl
+cinst gimp
+cinst syncback
+cinst 7zip.install
+cinst vlc
+
+
+# Manca Miktex, fastone-capture, office, codecompare e sqlcomplete
