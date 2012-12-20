eb1fe6110429bd3409f58d08f2d9be82dd306cf2
Federico Rota <federico.rota01@gmail.com>
2012-12-20 08:50:38 +0100

    Unlocker e altri

--- ChocoInstalla.ps1
+++ ChocoInstalla.ps1
@@ -6,20 +6,28 @@ iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
 
 cinst KickAssVim
 cinst GoogleChrome
-cinst skype
+#cinst skype
 cinst git.install
 cinst notepadplusplus.install
 cinst ferventcoder.chocolatey.utilities
 cinst keepassx
-cinst dropbox
 cinst windirstat
 cinst winrar
 cinst Firefox
-cinst toggl
+#cinst toggl
 cinst gimp
 cinst syncback
 cinst 7zip.install
+cinst Unlocker
+#cinst GoogleTalk
+cinst wim2vhd
+cinst grepwin
 cinst vlc
+cinst gow
+cinst python 
+cinst mongodb
 
+#cinst dropbox
 
 # Manca Miktex, fastone-capture, office, codecompare e sqlcomplete
+
